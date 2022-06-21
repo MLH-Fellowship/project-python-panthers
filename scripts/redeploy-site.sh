b22900af7281f9cdfa1fe6b7e7af0ccf76f52228
Joshua Adebayo <85533331+Epicskylegend@users.noreply.github.com>
2022-06-21 17:24:47 -0500

    Update redeploy-site.sh

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,3 +1,7 @@
+tmux kill-server
+cd .\project-python-panthers\
 git fetch
 git reset origin/main --hard
 source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+tmux new -ds flask-josh's-portfolio 'flask run --host=0.0.0.0;
