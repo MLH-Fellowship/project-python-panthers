dea46725860714078ac6941e2b37b9086fa12263
Epicskylegend <adeba003@gmail.com>
2022-06-24 09:58:23 -0500

    Moving files

--- app/templates/scrap/redeploy-site.sh
+++
@@ -1,8 +0,0 @@
-tmux kill-server
-cd .\project-python-panthers\
-git fetch
-git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-tmux new -s myportfolio 
-flask run --host=0.0.0.0;
