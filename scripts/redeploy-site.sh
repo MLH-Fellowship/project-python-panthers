c31cc75ff9d0457aeed74b14add186e185ad6687
Epicskylegend <adeba003@gmail.com>
2022-07-08 15:53:34 -0500

    Made updates to redeploy script file

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,8 +1,7 @@
 cd project-python-panthers/
 git fetch
 git reset origin/main --hard
-`docker compose -f docker-compose.prod.yml down`
-`docker compose -f docker-compose.prod.yml up -d --build`
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
 source python3-virtualenv/bin/activate
-systemctl daemon-reload
-systemctl restart myportfolio
+
